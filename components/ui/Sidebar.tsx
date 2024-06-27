7dfa7c9f474daa79b0f04e4e30553d4ab5a5edad
nafi-rahman <nafirahman16@gmail.com>
2024-06-28 02:30:33 +0600

    nav bar
    done with mobile and full view navigation

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -10,9 +10,9 @@ const Sidebar = () => {
 const pathname = usePathname();
   return (
     <section className='sticky left-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
-    <div className='flex flex- flex-col gap-6'>
+    <div className='flex flex-1 flex-col gap-6'>
         {sidebarLinks.map((link)=> {
-            const isActive = pathname === link.route || pathname.startsWith(link.route);
+            const isActive = pathname === link.route || pathname.startsWith(`${link.route}/`);
             return (
                 <Link href={link.route} key={link.label} className={cn('flex gap-4 items-center p-4 rounded-lg justify-start', {
                     'bg-blue-1': isActive,
