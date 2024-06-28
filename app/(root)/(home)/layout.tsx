a2ced0682d57b753f12569db9f6707265ac7ac62
Ammar Mohammad <ammaribnmohammad@gmail.com>
2024-06-28 23:52:45 +0600

    Home and resusable modal done

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,25 +1,28 @@
+import { Metadata } from 'next';
+import { ReactNode } from 'react';
 
-import Navbar from '@/components/ui/Navbar'
-import Sidebar from '@/components/ui/Sidebar'
-import React , { ReactNode } from 'react'
+import Navbar from '@/components/ui/Navbar';
+import Sidebar from '@/components/ui/Sidebar';
 
-const HomeLayout= ({children}: {children: ReactNode}) => {
-    return (
-      <main className="relative">
-            <Navbar/> 
-            <div className='flex'>
-                <Sidebar/>
+export const metadata: Metadata = {
+  title: 'YOOM',
+  description: 'A workspace for your team, powered by Stream Chat and Clerk.',
+};
 
-                <section className='flex-mid-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'>
-                    <div className='w-full '>
-                        {children}
-                    </div>
-                </section>
-            </div>
-          
-          
-      </main>
-    )
-  }
+const RootLayout = ({ children }: Readonly<{children: ReactNode}>) => {
+  return (
+    <main className="relative">
+      <Navbar />
 
-export default HomeLayout
+      <div className="flex">
+        <Sidebar />
+        
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
+          <div className="w-full">{children}</div>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default RootLayout;
--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import Image from 'next/image';
-
 import { cn } from '@/lib/utils';
 
 interface HomeCardProps {
@@ -12,14 +11,15 @@ interface HomeCardProps {
   handleClick?: () => void;
 }
 
+
+
 const HomeCard = ({ className, img, title, description, handleClick }: HomeCardProps) => {
   return (
-    <section
+    <div
       className={cn(
-        'bg-orange-1 px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer',
-        className
-      )}
-      onClick={handleClick}
+        ' px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer',
+        className)}
+        onClick={handleClick}
     >
       <div className="flex-center glassmorphism size-12 rounded-[10px]">
         <Image src={img} alt="meeting" width={27} height={27} />
@@ -27,9 +27,9 @@ const HomeCard = ({ className, img, title, description, handleClick }: HomeCardP
       
       <div className="flex flex-col gap-2">
         <h1 className="text-2xl font-bold">{title}</h1>
-        <p className="text-lg font-normal">{description}</p>
+        <p className="text-lg font-normal"> {description}</p>
       </div>
-    </section>
+    </div>
   );
 };
 
